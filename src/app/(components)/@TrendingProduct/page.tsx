500c3dd83a0a908857368155e13fd4ed913a1f2e
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2025-07-12 11:48:47 +0530

    Improved Trending Products Styles

--- src/app/(components)/@TrendingProduct/page.tsx
+++ src/app/(components)/@TrendingProduct/page.tsx
@@ -1,6 +1,8 @@
 import { BiTrendingUp } from "react-icons/bi";
 import { DummyData } from "@/lib/dummyData";
 import ProductCard from "../@ProductCard/page";
+import { IoIosArrowRoundForward } from "react-icons/io";
+import Link from "next/link";
 
 export default function TrendingProduct() {
     return (
@@ -12,8 +14,11 @@ export default function TrendingProduct() {
             <h1 style={{ fontFamily: 'var(--font-playfair)' }} className="text-5xl font-bold">Trending <span className="bg-gradient-to-r from-purple-700 to-pink-600 bg-clip-text text-transparent">Products</span></h1>
             <p className="text-xl leading-relaxed text-wrap">Discover the most coveted items loved by our community of discerning shoppers</p>
             <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-8 px-8 pt-10">
-                {DummyData.map((product, index) => <ProductCard key={index} product={product} />)}
+                {DummyData.slice(0,4).map((product, index) => <ProductCard key={index} product={product} />)}
             </div>
+            <Link href={"/products"}>
+                <button className="backdrop-blur-xl bg-white/20 px-6 py-3 rounded-full border-1 border-white text-gray-900 font-semibold text-xs my-16 cursor-pointer flex gap-2 justify-center items-center group">View All Trending Products<IoIosArrowRoundForward size={24} className="group-hover:translate-x-3 transition-all duration-300" /></button>
+            </Link>
         </div>
     )
 }
