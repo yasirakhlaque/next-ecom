460e947bcf583134898e898ca46c0cdfdb67188e
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2025-07-12 21:13:15 +0530

    Added more options for profile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,11 @@
+"use client"
 import Link from "next/link"
-import { FaSearch } from "react-icons/fa"
+import { useState } from "react"
+import { BsBoxArrowRight } from "react-icons/bs"
+import { FaRegUserCircle, FaSearch } from "react-icons/fa"
+import { FaBoxesStacked } from "react-icons/fa6"
 import { FiHeart, FiSun, FiUser } from "react-icons/fi"
+import { IoSettingsOutline } from "react-icons/io5"
 import { LuShoppingBag } from "react-icons/lu"
 
 export default function Navbar() {
@@ -10,6 +15,7 @@ export default function Navbar() {
         { name: "Categories", link: "/categories" },
         { name: "About", link: "/about" },
     ]
+    const [selectedOption, setSelectedOption] = useState(false);
     return (
         <nav className="sticky top-5 z-50 backdrop-blur-xl bg-white/20 border border-white/30 rounded-2xl shadow-2xl shadow-black/10 mx-4 mt-4 px-6 py-6 justify-between items-center text-gray-700 gap-7 hidden md:flex">
             <div className="flex justify-center items-center gap-2">
@@ -32,20 +38,45 @@ export default function Navbar() {
                 <input type="search" name="search" id="search" placeholder="Search Premium Products...." className="outline-none w-full bg-transparent placeholder-gray-500" />
             </div>
             <div className="gap-4 flex text-gray-800">
-                <button className="rounded-full bg-white/40 backdrop-blur-sm border border-white/40 p-3 hover:bg-white/60 transition-all duration-300">
+                <button className="rounded-full bg-white/40 backdrop-blur-sm border border-white/40 p-3 hover:bg-gray-300 transition-all duration-300 ">
                     <FiSun />
                 </button>
-                <button className="rounded-full bg-white/40 backdrop-blur-sm border border-white/40 p-3 relative hover:bg-white/60 transition-all duration-300">
+                <button className="rounded-full bg-white/40 backdrop-blur-sm border border-white/40 p-3 relative hover:bg-gray-300 transition-all duration-300 ">
                     <div className="absolute font-semibold bg-gradient-to-r from-pink-500 to-rose-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center -top-2 -right-1 shadow-lg">2</div>
                     <FiHeart />
                 </button>
-                <button className="rounded-full bg-white/40 backdrop-blur-sm border border-white/40 p-3 relative hover:bg-white/60 transition-all duration-300">
+                <button className="rounded-full bg-white/40 backdrop-blur-sm border border-white/40 p-3 relative hover:bg-gray-300 transition-all duration-300 ">
                     <div className="absolute font-semibold bg-gradient-to-r from-indigo-500 to-purple-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center -top-2 -right-1 shadow-lg">9</div>
                     <LuShoppingBag />
                 </button>
-                <button className="rounded-full bg-white/40 backdrop-blur-sm border border-white/40 p-3 hover:bg-white/60 transition-all duration-300">
-                    <FiUser />
-                </button>
+                <div>
+                    <button className="rounded-full bg-white/40 backdrop-blur-sm border border-white/40 p-3 hover:bg-gray-300 transition-all duration-300" onClick={() => setSelectedOption(!selectedOption)}>
+                        <FiUser />
+                    </button>
+                    {selectedOption &&
+                        <div>
+                            <div className="absolute right-0 top-20 bg-white/90 backdrop-blur-xl border border-white/50 rounded-lg shadow-2xl shadow-black/20 p-2 w-30 text-sm">
+                                <ul className="flex flex-col gap-1 text-gray-800">
+                                    <li className="flex items-center gap-1 hover:text-indigo-600 hover:bg-gray-200 pl-1 py-1 rounded-lg transition-colors duration-300">
+                                        <FaRegUserCircle />
+                                        <Link href="/user/profile">Profile</Link>
+                                    </li>
+                                    <li className="flex items-center gap-1 hover:text-indigo-600 hover:bg-gray-200 pl-1 py-1 rounded-lg transition-colors duration-300">
+                                        <FaBoxesStacked />
+                                        <Link href="/user">Dashboard</Link>
+                                    </li>
+                                    <li className="flex items-center gap-1 hover:text-indigo-600 hover:bg-gray-200 pl-1 py-1 rounded-lg transition-colors duration-300">
+                                        <IoSettingsOutline />
+                                        <Link href="/settings">Settings</Link>
+                                    </li>
+                                    <li className="flex items-center gap-1 hover:text-indigo-600 hover:bg-gray-200 pl-1 py-1 rounded-lg transition-colors duration-300">
+                                        <BsBoxArrowRight />
+                                        <Link href="/logout">Logout</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>}
+                </div>
             </div>
         </nav>
     )
