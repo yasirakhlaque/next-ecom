70e4e0c6a3cbd1c27e0d7905375d1aa6b997c346
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2025-07-13 13:17:09 +0530

    added fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import { Inter,Playfair_Display } from "next/font/google";
+import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 const playfair = Playfair_Display({ subsets: ["latin"], variable: "--font-playfair" })
@@ -17,9 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} ${playfair.variable} antialiased`}
-      >
+      <body className={`${inter.className} ${playfair.variable} antialiased bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100`}  >
+        <Navbar />
         {children}
       </body>
     </html>
