ca46202c7fb39de053772c9676fdc2864a81c763
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2025-07-13 13:17:26 +0530

    Merge Components To App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import HeroSection from "@/components/HeroSection";
-import Navbar from "@/components/Navbar";
-import TrendingProduct from "@/components/TrendingProduct";
+import TrendingProduct from "./(components)/@TrendingProduct/page";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100 px-2 py-2 text-gray-600">
-      <Navbar />
       <HeroSection />
       <TrendingProduct />
     </div>
