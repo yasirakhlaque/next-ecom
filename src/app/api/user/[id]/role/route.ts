c50e6f4f5d5df467a9cce3c8c033397f148735e5
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2025-07-14 22:21:43 +0530

    added promise as params type

--- src/app/api/user/[id]/role/route.ts
+++ src/app/api/user/[id]/role/route.ts
@@ -3,10 +3,10 @@ import { db } from "@/lib/db";
 
 export async function GET(
     req: NextRequest, 
-    { params }: { params: { id: string } }
+    { params }: { params: Promise<{ id: string }> }
 ) {
     try {
-        const { id } = params;
+        const { id } = await params;
         
         const user = await db.user.findUnique({
             where: { id: id },
