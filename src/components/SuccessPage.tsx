d132b305e42c8073c232e12b43da4325cb3549aa
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2025-07-30 16:55:52 +0530

    Added Success Page

---
+++ src/components/SuccessPage.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { TbRosetteDiscountCheckFilled } from "react-icons/tb";
+
+export default function SuccessPage({ orderId }: { orderId: string | null }) {
+    const router = useRouter();
+    useEffect(() => {
+        const OrderPageRedirect = () => {
+            setTimeout(() => {
+                router.push('/user/order');
+            }, 3000);
+        }
+        OrderPageRedirect();
+    }, [])
+    return (
+        <div className="min-h-screen justify-center items-center bg-green-700 flex flex-col gap-4 p-6 text-white">
+            <div className="success">
+                <TbRosetteDiscountCheckFilled size={50}/>
+            </div>
+            <h1 className="text-2xl font-bold">Order #{orderId} placed successfully!</h1>
+        </div>
+    )
+}
